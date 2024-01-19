fcb4c02feec6bec7ccc97cd1bf19b7e32e4f2eb7
Dale Marttinen <dmarttinen@gmail.com>
2024-01-19 11:15:56 -0800

    copied command from pipeline config file

--- build.ps1
+++ build.ps1
@@ -155,11 +155,12 @@ Function PackageMaui {
 	 $signingStorePass = [Environment]::GetEnvironmentVariable("signingStorePass","User")
 	 $signingKeyPass = [Environment]::GetEnvironmentVariable("signingKeyPass","User")
     exec{
-        & dotnet publish $mauiProjectPath -nologo --no-restore -v $verbosity --configuration $projectConfig -f net8.0-android -p:AndroidPackageFormat=aab -p:AndroidKeyStore=True `
-		-p:AndroidSigningKeyStore=$keystoreFilePath `
-		-p:AndroidSigningStorePass=$signingStorePass `
-		-p:AndroidSigningKeyAlias="release" `
-		-p:AndroidSigningKeyPass=$signingKeyPass
+        # & dotnet publish $mauiProjectPath -nologo --no-restore -v $verbosity --configuration $projectConfig -f net8.0-android -p:AndroidPackageFormat=aab -p:AndroidKeyStore=True `
+		# -p:AndroidSigningKeyStore=$keystoreFilePath `
+		# -p:AndroidSigningStorePass=$signingStorePass `
+		# -p:AndroidSigningKeyAlias=release `
+		# -p:AndroidSigningKeyPass=$signingKeyPass
+		& dotnet publish -c $projectConfig -f net8.0-android -p:AndroidPackageFormat=aab -p:AndroidKeyStore=True -p:AndroidSigningKeyStore=$keystoreFilePath -p:AndroidSigningStorePass=$signingStorePass -p:AndroidSigningKeyAlias=release -p:AndroidSigningKeyPass=$signingKeyPass
     }
 	exec{
 		& dotnet-octo pack --id "$projectName.Maui" --version $version --basePath $mauiProjectPath\bin\$projectConfig\net8.0-android\publish  --include "*-Signed.aab" --outFolder $build_dir --overwrite
