ac3c5ee14f963f686bbe9e43f28f874585b16c8d
Richard Ram <richard.ram@clear-measure.com>
2024-03-28 10:36:45 -0700

    feat(ios) - removed http calls, updated nugets, removed wpf

--- src/UI/Maui/MauiProgram.cs
+++ src/UI/Maui/MauiProgram.cs
@@ -21,28 +21,6 @@ namespace UI.Maui
             builder.Services.AddMauiBlazorWebView();
             builder.Services.AddScoped<IUiBus>(provider => new MvcBus(NullLogger<MvcBus>.Instance));
 
-            try
-            {
-                var assembly = Assembly.GetExecutingAssembly();
-                var stream = assembly.GetManifestResourceStream("UI.Maui.Resources.Raw.appsettings.json") ?? 
-                             throw new FileNotFoundException("The appsettings.json file was not found.");
-
-                using (stream)
-                {
-                    var config = new ConfigurationBuilder().AddJsonStream(stream).Build();
-                    builder.Configuration.AddConfiguration(config);
-                }
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
-
-            var baseAddress = builder.Configuration.GetValue<string>("BaseAddress") 
-                                 ?? (DeviceInfo.Platform == DevicePlatform.Android ? "https://10.0.2.2:7174" : "https://localhost:7174");
-            //baseAddress = "https://prod-ui.kindriver-b264a607.southcentralus.azurecontainerapps.io";
-            var handler = new HttpsClientHandlerService();
-            builder.Services.AddSingleton(sp => new HttpClient(handler.GetPlatformMessageHandler()) { BaseAddress = new Uri(baseAddress) });
 #if DEBUG
             builder.Services.AddBlazorWebViewDeveloperTools();
             builder.Logging.AddDebug();
