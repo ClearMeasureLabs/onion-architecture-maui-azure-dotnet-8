a13b10e572d227b62a5d0b6c2881ab45946aa7fa
Richard Ram <richard.ram@clear-measure.com>
2024-04-23 12:13:44 -0700

    feat(client-otel) - update nullable

--- src/Core/ConfigurationModel.cs
+++ src/Core/ConfigurationModel.cs
@@ -2,6 +2,6 @@
 {
     public class ConfigurationModel
     {
-        public string AppInsightsConnectionString { get; set; }
+        public string? AppInsightsConnectionString { get; set; }
     }
 }
